8790461e45241285077ef965e9a381011a5569a6
rajectedguy <rajectedguy@gmail.com>
2026-08-18 22:08:06 +0530

    1. Count all Digits of a Number 2. Reverse a number 3. Palindrome Number 4. GCD of Two Numbers 5. Check if the Number is Armstrong 6. Print all Divisors 7. Check for Prime Number

--- BasicMaths/src/CountDigits.java
+++ BasicMaths/src/CountDigits.java
@@ -6,7 +6,6 @@ public class CountDigits {
         int n = sc.nextInt();
         int count = 0;
         while (n != 0) {
-            int digit = n % 10;
             n /= 10;
             count++;
         }
